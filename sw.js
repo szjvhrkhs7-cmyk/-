ea62206544b883904e666a3e893d3d6b908ab786
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-07-18 13:23:26 +0300

    Refresh PWA cache after ruble spacing fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'puls-v6';
+const CACHE = 'puls-v7';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './utils.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => event.waitUntil(
