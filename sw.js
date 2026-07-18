71d984bd81973db09f621ac37aad8680e20a179b
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-07-18 11:27:25 +0300

    Refresh PWA cache for budget planner

--- sw.js
+++ sw.js
@@ -1,9 +1,19 @@
-const CACHE = 'puls-v4';
+const CACHE = 'puls-v5';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './utils.js', './manifest.webmanifest', './icon.svg'];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+
+self.addEventListener('install', (event) => event.waitUntil(
+  caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+));
+
+self.addEventListener('activate', (event) => event.waitUntil(
+  caches.keys()
+    .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+    .then(() => self.clients.claim())
+));
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(fetch(event.request).then((response) => {
     const copy = response.clone();
     caches.open(CACHE).then((cache) => cache.put(event.request, copy));
